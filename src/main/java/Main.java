d1f180e7dbc2b5bf00e268a94c5e0041551c3267
Calvin <calvin@Zipcoders-MacBook-Pro.local>
2022-10-26 18:26:16 -0400

    created input reader and conditional output code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,38 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args ){
 
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.println("Enter your name: ");
+
+        String name = userInput.nextLine();
+
+        System.out.println("Your name is: " + name);
+
+        name = name.toLowerCase();
+
+        if (name.equals("alice") || name.equals("bob")) {
+            System.out.println("Hello there!");
+        }
+        else {
+            System.out.println("Good-bye");
+        }
+
+        /*
+        Objective:
+        Write a program which prompts the user to input his/her name.
+        The program should greet users whose names are 'Alice' and 'Bob'.
+
+        Instructions:
+        Understand how to get input from user
+        Create conditional to check against Alice and Bob
+        Print greeting to screen if Alice or Bob are true
+
+        */
     }
 }
